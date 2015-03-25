e35b01d11ee84a9fd6c2f67ce3ee7f7d66285404
Scott Flaherty <ei710c5@scottflahertymb15.mckesson.com>
2015-03-25 08:23:30 -0400

    Adding test framework

---
+++ tests/spec/LoggerSpec.js
@@ -0,0 +1,21 @@
+describe("Logger", function() {
+    var d;
+
+    beforeEach(function() {
+
+    });
+
+    afterEach(function() {
+
+    });
+
+    describe("put test topic here", function() {
+        it("put test item here", function() {
+            //var origCount = d.count();
+            //d.add('Howard', 49);
+            //var newCount = d.count();
+            //
+            //expect(newCount).toBeGreaterThan(origCount);
+        });
+    });
+});
